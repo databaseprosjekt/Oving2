fa7696d7ff74f5d05dc13920c997044aa976ab7b
paal <pciverser@stud.ntnu.no>
2017-03-06 13:33:40 +0100

    Added new quries

--- prosjekt databaser/src/DbQuerys.java
+++ prosjekt databaser/src/DbQuerys.java
@@ -1,14 +1,30 @@
 
 public class DbQuerys {
 
-private String treningsSQL = ""; 
-	
 	public DbQuerys(){
 		
 	}
-
-	public String getTrening(){
-		return treningsSQL;
+	public String getTrening(int id){
+		return "SELECT * FROM pciverse_tdt4145.Treningsøkt WHERE ØktID =" + id;
 	}
-	
+	public String getResultat(String type){
+		return "SELECT * FROM pciverse_tdt4145.Resultat" + type; 
+	}
+	public String getAlleGrupper(){
+		return "SELECT * FROM pciverse_tdt4145.Gruppe;"; 
+	}
+	public String getSubGrupper(int groupID){
+		return "SELECT Navn, AltGruppeID "
+				+ "FROM `pciverse_tdt4145`.`Gruppe`,`pciverse_tdt4145`.`Gruppe_Gruppe` "
+				+ "WHERE `pciverse_tdt4145`.`Gruppe`.GruppeID = `pciverse_tdt4145`.`Gruppe_Gruppe`.GruppeID "
+				+ "AND `pciverse_tdt4145`.`Gruppe`.GruppeID = '"+Integer.toString(groupID)+"';";
+	}
+	public String getIkkeSubGrupper(){
+		return "SELECT * "
+				+ "FROM pciverse_tdt4145.Gruppe G "
+				+ "WHERE NOT EXISTS "
+					+ "(SELECT *"
+					+ "FROM pciverse_tdt4145.Gruppe_Gruppe GG "
+					+ "WHERE G.GruppeID = GG.AltGruppeID);";
+	}
 }
