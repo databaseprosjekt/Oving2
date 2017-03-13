055396d4caa933cc27c90fea52e11f851a414265
Paalch <pciverser@stud.ntnu.no>
2017-03-13 11:37:11 +0100

    added method getOvelseFraGruppe

--- prosjekt databaser/src/DbView.java
+++ prosjekt databaser/src/DbView.java
@@ -1,5 +1,6 @@
 //STEP 1. Import required packages
 import java.sql.*;
+import java.util.ArrayList;
 
 public class DbView {
    // JDBC driver name and database URL
@@ -9,61 +10,24 @@ public class DbView {
    //  Database credentials
    static final String USER = "pciverse_tdt4145";
    static final String PASS = "password";
-
-   public static void main(String[] args) {
+   
+   private DbQuerys querys; 
+   
    Connection conn = null;
    Statement stmt = null;
+
+   public static void main(String[] args) {
    try{
       //STEP 2: Register JDBC driver
       Class.forName("com.mysql.jdbc.Driver");
-
-      //STEP 3: Open a connection
-      System.out.println("Connecting to database...");
-      conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-      //STEP 4: Execute a query
-      System.out.println("Creating statement...");
-      stmt = conn.createStatement();
-      String sql;
-      sql = "SELECT * FROM pciverse_tdt4145.Poster";
-      ResultSet rs = stmt.executeQuery(sql);
-
-      //STEP 5: Extract data from result set
-      while(rs.next()){
-         //Retrieve by column name
-         int id  = rs.getInt("PostNr.");
-         String beskrivelse = rs.getString("Beskrivelse");
-
-         //Display values
-         System.out.print("ID: " + id + " ");
-         System.out.println("Beskrivelse: " + beskrivelse);
-      }
-      //STEP 6: Clean-up environment
-      rs.close();
-      stmt.close();
-      conn.close();
    }catch(SQLException se){
-      //Handle errors for JDBC
-      se.printStackTrace();
-   }catch(Exception e){
-      //Handle errors for Class.forName
-      e.printStackTrace();
-   }finally{
-      //finally block used to close resources
-      try{
-         if(stmt!=null)
-            stmt.close();
-      }catch(SQLException se2){
-      }// nothing we can do
-      try{
-         if(conn!=null)
-            conn.close();
-      }catch(SQLException se){
-         se.printStackTrace();
-      }//end finally try
-   }//end try
-   System.out.println("Goodbye!");
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+   }
+   
 }//end main
+   
+ /*  
    public void addTreningsØkt(Date date, Time time, int varighet, int form, int prestasjon, String notat){
       String sql = "INSERT INTO Treningsøkt VALUES (" + date + ", " + time + ", " + varighet + ", " + form + ", " + prestasjon + ", " + notat + ");";
       // PROBLEM: Her kan det bli noe problem hvis java ikke vil konvertere int/date/time til string. pls no.
@@ -86,5 +50,59 @@ public class DbView {
       ResultSet rs = stmt.executeQuery(sql); // THIS IS AN WRONG atm PROBLEM
    }
 
+*/
+   public ArrayList<ArrayList> getOvelseFraGruppe(int groupID){
+	  ResultSet result =  enquire(querys.getOvelseFraGruppe(groupID));
+	  ArrayList<ArrayList> arrayResult = new ArrayList<ArrayList>();
+	  arrayResult.add(new ArrayList<String>());
+	  arrayResult.add(new ArrayList<Integer>());
+	  
+	  try {
+		while(result.next()) {
+			  arrayResult.get(0).add(result.getString("Ovelse.Navn"));
+			  arrayResult.get(1).add(result.getString("Ovelse.�velsesID"));
+			}
+		  return arrayResult;
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  return null;
+   }
+	public ResultSet enquire(String sql){
+	      //STEP 3: Open a connection
+		try {
+	      conn = DriverManager.getConnection(DB_URL,USER,PASS);
 
+	      //STEP 4: Execute a query
+	      stmt = conn.createStatement();
+	      ResultSet rs = stmt.executeQuery(sql);
+
+	      //STEP 6: Clean-up environment
+	      rs.close();
+	      stmt.close();
+	      conn.close();
+	      
+	      return rs;
+
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
+	   }finally{
+	      //finally block used to close resources
+	      try{
+	         if(stmt!=null)
+	            stmt.close();
+	      }catch(SQLException se2){
+	      }// nothing we can do
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	         se.printStackTrace();
+	      }//end finally try
+	   }//end try
+		System.out.println("Database error");
+	  return null; 
+	}
 }//end FirstExample
